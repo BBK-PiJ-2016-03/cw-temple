d8faf5078356420d80f9eb6a5c25267d5d66416b
Alexander Worton <alexander@ajsworton.com>
2017-02-05 11:38:17 +0000

    Fleshed out required methods for navigator and cavernMap

--- src/student/CavernMap.java
+++ src/student/CavernMap.java
@@ -4,4 +4,13 @@ package student;
  * Created by Alexander Worton on 05/02/2017.
  */
 public interface CavernMap {
+
+    /**
+     * add a new node to the map
+     * @param node
+     */
+    void addNode(CavernNode node);
+
+
+
 }
--- src/student/Navigator.java
+++ src/student/Navigator.java
@@ -1,8 +1,27 @@
 package student;
 
+import java.util.Queue;
+
 /**
  * Created by Alexander Worton on 02/02/2017.
  */
 public interface Navigator {
+    /**
+     * set starting node
+     * @param node
+     */
+    void setStartNode(CavernNode node);
 
+    /**
+     * set destination node
+     * @param node
+     */
+    void setDestinationNode(CavernNode node);
+
+    /**
+     * get the sequence of nodes to form the path from start to destination,
+     * including both starting and destination nodes
+     * @return a queue of cavernNodes
+     */
+    Queue<CavernNode> getPathFromStartToDestination();
 }
